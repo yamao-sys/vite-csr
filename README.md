dcf47f446c30e61b7e7d6a3e0bc094bd1e5b6198
yamao-sys <yamaopanku@gmail.com>
2024-07-28 11:14:46 +0900

    APIサーバ, Vite × Reactの初期構築

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # vite-csr
-Vite × ReactによるCSRの検証
+
+Vite × React による CSR の検証
+
+## ESLint, Prettier の導入
+
+https://qiita.com/Stellarium/items/095ca74299a50016c321
---
+++ api_server/app/src/auth/auth.guard.ts
@@ -0,0 +1,29 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = request.cookies['token'];
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET,
+      });
+      request['user'] = payload;
+    } catch (error) {
+      console.error(error);
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+}
